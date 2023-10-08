b9592ba0cc352ed8f88c24439297a25366c04792
Edgar Alfredo Rivera Díaz <edgarafedo123@gmail.com>
2023-10-08 02:44:48 -0600

    interfaz dark

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,24 +2,25 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
+    
     <aside
       id="logo-sidebar"
-      className="fixed top-0 right-0 z-00 w-64 h-screen  transition-transform translate-x-full bg-white sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+      className="fixed top-0 right-0 z-00 w-64 h-screen  transition-transform translate-x-o bg-gray 800 sm:translate-x-0 bg-gray-800 border-gray-700"
       aria-label="Sidebar"
     >
-      <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-green-700">
+      <div className="h-full px-3 pb-4 overflow-y-auto bg-white bg-green-700">
         <ul className="space-y-2 font-medium">
         <li>
-            <a href="#" class="flex items-center p-1 text-gray-900 rounded-lg dark:text-white hover:bg-lime-300 dark:hover:bg-lime-300 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-lime-300 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+            <a href="#" class="flex items-center p-1 text-gray-900 rounded-lg text-white hover:text-gray-900 hover:bg-lime-300 hover:bg-lime-300 group">
+               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 text-lime-300 group-hover:text-gray-900 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
                </svg>
                <span class="flex-1 ml-3 whitespace-nowrap">Sign In</span>
             </a>
          </li>
          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-lime-300 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-lime-300 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <a href="#" class="flex items-center p-1 text-gray-900 rounded-lg text-white hover:text-gray-900 hover:bg-gray-100 hover:bg-lime-300 group">
+               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 text-lime-300 group-hover:text-gray-900 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
                   <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
                   <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
@@ -29,10 +30,10 @@ const Sidebar = () => {
          </li>
           <li>
             <a href="#"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:text-gray-900 dark:hover:bg-lime-300 group"
+              className="flex items-center p-2 text-gray-900 rounded-lg text-white hover:text-gray-900 hover:text-gray-900 hover:bg-lime-300 group"
             >
               <svg
-                className="w-5 h-5 text-white-500 transition duration-75 dark:text-lime-300 group-hover:text-gray-900 dark:group-hover:text-black"
+                className="w-5 h-5 text-white-500 transition duration-75 text-lime-300 group-hover:text-gray-900 group-hover:text-gray-900"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -47,8 +48,9 @@ const Sidebar = () => {
         
         
          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-lime-300 dark:hover:bg-lime-300 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-lime-300 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+ <a href="#"
+              className="flex items-center p-2 text-gray-900 rounded-lg text-white  hover:text-gray-900 hover:bg-lime-300 group"
+            >               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 text-lime-300 group-hover:text-gray-900 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                   <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                </svg>
                <span class="flex-1 ml-3 whitespace-nowrap">desarrolladores que sigues</span>
@@ -56,8 +58,8 @@ const Sidebar = () => {
          </li>
         
          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-lime-300 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-lime-300 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg text-white hover:text-gray-900 hover:bg-gray-100 hover:bg-lime-300 group">
+               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 text-lime-300 group-hover:text-gray-900 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
                </svg>
                <span class="flex-1 ml-3 whitespace-nowrap">Inbox</span>
